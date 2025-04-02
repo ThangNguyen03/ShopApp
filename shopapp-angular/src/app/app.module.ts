e1d2a80721e8e53d728e03a7614fb1ac3d7bcbf3
quyetthang <quyetthang@vnpt.vn>
2025-04-02 22:45:16 +0700

    add login

--- shopapp-angular/src/app/app.module.ts
+++ shopapp-angular/src/app/app.module.ts
@@ -3,18 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { OrderComponent } from './order/order.component';
+import { DetailProductComponent } from './detail-product/detail-product.component';
+import { OrderConfirmComponent } from './order-confirm/order-confirm.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 
 @NgModule({
   declarations: [
     HomeComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    OrderComponent,
+    DetailProductComponent,
+    OrderConfirmComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [HomeComponent] // Khởi chạy trước tiên HomeComponent khi cho và bootstrap
+  bootstrap: [
+    // HomeComponent,
+    // OrderComponent,
+    // OrderConfirmComponent,
+    // LoginComponent,
+    RegisterComponent
+  ] // Khởi chạy trước tiên HomeComponent khi cho và bootstrap
 })
 export class AppModule { }
